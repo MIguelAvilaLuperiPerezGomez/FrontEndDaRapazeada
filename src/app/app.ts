3257b27af2b8603abe6cb194f74d13fd2dcffaf3
Miguel <migueltkd63@gmail.com>
2026-04-27 00:36:08 -0300

    adicionando loading

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,40 +1,45 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { Tarefa } from "./tarefa";
 import { HttpClient } from '@angular/common/http';
- 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
   styleUrl: './app.css'
 })
- 
+
 export class App implements OnInit {
   protected readonly title = signal('TODOapp');
   apiURL: string;
-  Tarefas = signal<Tarefa[]>([]);  // ✅ Apenas o Signal
- 
+  Tarefas = signal<Tarefa[]>([]);
+  carregando = signal(true); // ✅ adicionado
+
   constructor(private http: HttpClient) {
     this.apiURL = 'https://backenddarapazeada.onrender.com';
   }
- 
+
   ngOnInit() {
     this.READ_tarefas();
   }
- 
+
   CREATE_tarefa(descricaoNovaTarefa: string) {
     var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
     this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
       resultado => { console.log(resultado); this.READ_tarefas(); }
     );
   }
- 
+
   READ_tarefas() {
+    this.carregando.set(true); // ✅
     this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
-      resultado => this.Tarefas.set(resultado)  // ✅ .set() no Signal
+      resultado => {
+        this.Tarefas.set(resultado);
+        this.carregando.set(false); // ✅
+      }
     );
   }
- 
+
   DELETE_tarefa(tarefaAserRemovida: Tarefa) {
     const indice = this.Tarefas().indexOf(tarefaAserRemovida);
     const id = this.Tarefas()[indice]._id;
@@ -42,7 +47,7 @@ export class App implements OnInit {
       resultado => { console.log(resultado); this.READ_tarefas(); }
     );
   }
- 
+
   UPDATE_tarefa(tarefaAserModificada: Tarefa) {
     const indice = this.Tarefas().indexOf(tarefaAserModificada);
     const id = this.Tarefas()[indice]._id;
